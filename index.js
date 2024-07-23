35d1e83ed5d4fdd00d62eaf37a7e70b7bafb264d
rafi2k3 <rafickmohammed23@gmail.com>
2024-07-23 14:01:52 +0530

    this is my first website to upload in github

---
+++ index.js
@@ -0,0 +1,15 @@
+let sidenav=document.querySelector(".side-navbar")
+
+
+function shownavbar()
+{
+    sidenav.style.display="block"
+
+}
+function closenavbar()
+{
+    sidenav.style.display="none"
+
+
+
+}
